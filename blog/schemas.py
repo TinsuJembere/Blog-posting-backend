c51101e740aa36dc893fc4306fa2cdd4f88549bd
Tinsu Jembere <tinsaejembere4@gmail.com>
2025-11-10 10:12:15 +0300

    authentication added

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 class User(BaseModel):
     name: str
     email: str
@@ -30,6 +30,14 @@ class ShowBlog(Blog):
     class Config():                          
         orm_mode = True
 
+class Login(BaseModel):
+    username: str
+    password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
